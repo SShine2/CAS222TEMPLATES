bc5abaf18a90426beb16cbaf48b7ae0c47d17a4e
SShine2 <shoshi.a.s@gmail.com>
2017-05-18 21:37:44 -0700

    Create navbar.php

---
+++ UPDATED/includes/navbar.php
@@ -0,0 +1,18 @@
+<header>
+      <h1><span><img src="images/ACEintheHOLE.png" alt="ace in the hole multisport events logo red white black" class="logo"></span></h1>
+    <div id="nav-icon4">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    </header>
+      
+  <div class="menu" id="menuExpand">
+    <ul>
+        <li <?php if ($currentPage === 'Ace in the Hole Multisport Events | Home') {echo 'class="active"';} ?>><a href="contact.php">contact</a></li>
+      <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="faqs.php">faqs</a></li>
+        <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="prepare.php">prepare</a></li>
+        <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="course.php">course details</a></li>
+        <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="index.php">home</a></li>
+    </ul>
+  </div>
