08155f7da5280f9d05bc4d90dc0ed2ec6674304c
SShine2 <shoshi.a.s@gmail.com>
2017-05-18 21:39:09 -0700

    Add files via upload

---
+++ UPDATED/includes/footer.php
@@ -0,0 +1,9 @@
+</main>
+
+
+    <footer>
+    <p class="footer">Copyright &copy; Shoshi Small 2017</p>
+
+    </footer>
+</body>
+</html>
---
+++ UPDATED/includes/head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+    
+     <link rel="stylesheet" href="styles/reset.css">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Athiti|Cabin+Sketch:700|Julius+Sans+One" rel="stylesheet">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script type="text/javascript" src="scripts/scripts.js"></script>
+
+
+  <title>Ace In The Hole Multi Sport Events</title>
+
+</head>
