0e2d1b3c86b05758592712cfbff755ea55895d1a
SShine2 <shoshi.a.s@gmail.com>
2017-05-23 22:29:33 -0700

    Add files via upload

---
+++ UPDATES-2/includes/footer.php
@@ -0,0 +1,11 @@
+</main>
+
+
+    <footer>
+        <p class="footer"><a href="http://www.twitter.com/pcccas222"><img src="images/tweetfooter.png" alt="facebook logo icon social media" class="pics"></a>    <a href="http://www.facebook.com/cas222cascade"><img src="images/facefooter.png" alt="facebook logo icon social media" class="pics"></a></p>
+        <p class="footer">Ace in the Hole Multisport Events</p>
+    <p class="footer">Copyright &copy; Shoshi Small 2017</p>
+
+    </footer>
+</body>
+</html>
---
+++ UPDATES-2/includes/navbar.php
@@ -0,0 +1,18 @@
+<header>
+      <h1><span><img src="images/acelogo.gif" alt="ace in the hole multisport events logo red white black" class="logo"></span></h1>
+    <div id="nav-icon4">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    </header>
+      
+  <div class="menu" id="menuExpand">
+    <ul>
+        <li <?php if ($currentPage === 'Ace in the Hole Multisport Events | Home') {echo 'class="active"';} ?>><a href="contact.php">Contact</a></li>
+      <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="faqs.php">FAQs</a></li>
+        <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="prepare.php">Prepare</a></li>
+        <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="course.php">Course Details</a></li>
+        <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="index.php">Home</a></li>
+    </ul>
+  </div>
